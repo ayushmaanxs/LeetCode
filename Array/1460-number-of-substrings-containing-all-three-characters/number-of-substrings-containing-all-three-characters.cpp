4bb2121c47dedc6484a726db70e073b07eaa300e
Ayushmaan Shandilya <90828689+ayushmaanxs@users.noreply.github.com>
2026-05-04 16:06:24 +0530

    Time: 27 ms (21.75%) | Memory: 11.2 MB (31.76%) - LeetSync

--- Array/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ Array/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        unordered_map<char, int> f; // frequency map for a, b, c
         int n = s.size();
-        int low = 0;   // window start
-        int res = 0;   // final answer
+        unordered_map<char, int> f;
+        int low = 0;
+        int res = 0;
         for (int high = 0; high < n; high++) {
-            f[s[high]]++;  // expand window by including current char
-            // jab tak window me a, b, c teeno present hain
+            f[s[high]]++;
+            // jab tak window valid hai
             while (f['a'] > 0 && f['b'] > 0 && f['c'] > 0) {
-                // current window valid hai
-                // isliye is low se start hone wale
-                // saare substrings count kar sakte hain
-                // jo high se aage tak extend honge
-                res += (n - high);
-                // ab window ko shrink karte hain (left se)
+                res = res + (n - high); // store in
                 f[s[low]]--;
                 low++;
             }
