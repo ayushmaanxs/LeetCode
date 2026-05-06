3b71c27475f1c2483dd25861b88c8dcdedf14886
Ayushmaan Shandilya <90828689+ayushmaanxs@users.noreply.github.com>
2026-05-06 20:35:52 +0530

    Time: 20 ms (37.99%) | Memory: 107 MB (86.62%) - LeetSync

---
+++ Array/2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int n = garbage.size();
+        int totalTime = 0;
+        // Store last house index for each garbage type
+        int lastM = 0;
+        int lastP = 0;
+        int lastG = 0;
+        // Traverse all houses
+        for(int i = 0; i < n; i++) {
+            // Picking each garbage takes 1 minute
+            totalTime += garbage[i].size();
+            // Check each garbage type in current house
+            for(char ch : garbage[i]) {
+                if(ch == 'M') {
+                    lastM = i;
+                }
+                else if(ch == 'P') {
+                    lastP = i;
+                }
+                else {
+                    lastG = i;
+                }
+            }
+        }
+        // Add travel time for Metal truck
+        for(int i = 0; i < lastM; i++) {
+            totalTime += travel[i];
+        }
+
+        // Add travel time for Paper truck
+        for(int i = 0; i < lastP; i++) {
+            totalTime += travel[i];
+        }
+
+        // Add travel time for Glass truck
+        for(int i = 0; i < lastG; i++) {
+            totalTime += travel[i];
+        }
+
+        return totalTime;
+    }
+};
