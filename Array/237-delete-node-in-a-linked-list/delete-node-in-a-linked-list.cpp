0893e70563e0515af0044d417decb6c043c2d398
Ayushmaan Shandilya <90828689+ayushmaanxs@users.noreply.github.com>
2026-06-21 20:55:29 +0530

    Time: 5 ms (79.44%) | Memory: 12.3 MB (55.63%) - LeetSync

--- Array/237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ Array/237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -6,44 +6,10 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
 class Solution {
 public:
-    void deleteNode(ListNode* node) {
-        // p -> node which we want to delete
-        ListNode* p = node;
-        // q -> next node of p
-        ListNode* q = p->next;
-        /*
-            Since we cannot access previous node,
-            we copy next node's value into current node.
-
-            Example:
-
-            Before:
-            4 -> 5 -> 1 -> 9
-                 p    q
-
-            After copying:
-            4 -> 1 -> 1 -> 9
-        */
-        p->val = q->val;
-        /*
-            Now remove q from linked list.
-
-            p should directly point to node after q.
-
-            Before:
-            p -> q -> next
-
-            After:
-            p ------> next
-        */
-        p->next = q->next;
-
-        /*
-            Free memory of deleted node
-        */
-        delete(q);
+    void deleteNode(ListNode* target) {
+        target->val = target->next->val;
+        target->next = target->next->next;
     }
 };
